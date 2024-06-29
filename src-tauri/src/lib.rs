010d2632e5c55b5d2848b93cbd0e58287d17c986
Seferino Fernandez <seferinofz@gmail.com>
2024-06-29 13:37:07 -0700

    Added tauri-plugin-http to perform HTTP requests

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,6 +4,7 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_window_state::Builder::default().build())
+        .plugin(tauri_plugin_http::init())
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
